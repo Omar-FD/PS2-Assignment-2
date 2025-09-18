48d6a99cb8ff8303f329752b130696b7daf572e1
Omar-FD <s4165749@student.rmit.edu.au>
2025-09-18 20:36:54 +1000

    Implemented |playerpos.y-G_Y| ; Implemented |playerpox.z-G_Z| ; Implemented D(manhattan)

--- manhattan_dist.asm
+++ manhattan_dist.asm
@@ -15,17 +15,57 @@ NOT R3, R3
 ADD R3, R3, #1
 
 ;Add the playerpos.x && negated G_X
-ADD R3, R3, R0
+ADD R3, R0, R3
 REG
 
-;Get the absolute value
-BRzp NO_NEGATION
+;|playerpos.x - G_X|
+BRzp NO_X_NEGATION
 NOT R3, R3
 ADD R3, R3, #1
 
-NO_NEGATION
+;Get negative G_Y
+AND R4, R4, #0
+LD R4, G_Y
+NOT R4, R4
+ADD R4, R4, #1
+
+NO_X_NEGATION
+
+;playerpos.y - G_Y
+ADD R4, R1, R4
 REG
 
+;|playerpos.y - G_Y|
+BRzp NO_Y_NEGATION
+NOT R4, R4
+ADD R4, R4, #1
+
+NO_Y_NEGATION
+
+;Get negative G_Z
+AND R5, R5, #0
+LD R5, G_Z
+NOT R5, R5
+ADD R5, R5, #1
+
+;playerpos.z - G_Z
+ADD R5, R2, R5
+
+;|playerpos.z - G_Z|
+BRzp NO_Z_NEGATION
+NOT R5, R5
+ADD R5, R5, #1
+
+NO_Z_NEGATION
+
+;Get the Dmanhattan;
+AND R6, R6, #0
+ADD R6, R3, R4
+ADD R6, R6, R5
+
+
+
+REG
 HALT
 INBOUNDS    .STRINGZ "The player is within distance of the goal\n"
 OUTBOUNDS   .STRINGZ "The player is outside the goal bounds\n"
