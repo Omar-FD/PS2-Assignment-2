0e00f756f636da78333808149fa7f0c1ca818bfd
Omar-FD <s4165749@student.rmit.edu.au>
2025-09-23 21:12:15 +1000

    Made a start on the Terraform challenge. Made some initial division logic.

---
+++ terraform.asm
@@ -0,0 +1,32 @@
+.ORIG	x3000
+;Clear used regs
+AND R0, R0, #0
+AND R1, R1, #0
+AND R2, R2, #0
+AND R3, R3, #0
+;Set the stone block in reg 1
+ADD R3, R3, #1
+
+
+JSR DIV_SUB
+
+
+END_DIV
+REG
+HALT
+NUMBER_TO_CONVERT   .FILL	#10
+
+DIV_SUB
+;Clearing the reg to store the number and one for the counter
+    AND R4, R4, #0
+    AND R5, R5, #0
+    LD R4, NUMBER_TO_CONVERT
+
+    DIV_ACT
+    ADD R5, R5, #1
+    ADD R4, R4, #-2
+    BRp DIV_ACT
+    RET
+
+
+.END
