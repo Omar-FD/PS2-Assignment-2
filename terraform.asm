c1d86dd9d33dfc0ae7293ce155663b3ee88c36a8
Omar-FD <s4165749@student.rmit.edu.au>
2025-09-29 11:11:20 +1000

    Fixed issue with division and implemented remainders stored in R4

--- terraform.asm
+++ terraform.asm
@@ -4,30 +4,28 @@ AND R0, R0, #0
 AND R1, R1, #0
 AND R2, R2, #0
 AND R3, R3, #0
-;Set the stone block in reg 1
+;Set the stone block in reg 3
 ADD R3, R3, #1
 
 
+LD R4, NUMBER_TO_CONVERT
 JSR DIV_SUB
 
 
 END_DIV
 REG
 HALT
-NUMBER_TO_CONVERT   .FILL	#10
+NUMBER_TO_CONVERT   .FILL	#41
 
 DIV_SUB
 ;Clearing the reg to store the number and one for the counter
-    AND R4, R4, #0
-    AND R5, R5, #0
-    LD R4, NUMBER_TO_CONVERT
-
-    DIV_ACT
-    ADD R4, R4, #-2
+    ADD R5, R4, #-2
     BRn END_DIV
-    ADD R5, R5, #1
-    BRp DIV_ACT
 
+    ADD R4, R4, #-2
+
+    ADD R6, R6, #1
+    BR DIV_SUB
     RET
 
 
